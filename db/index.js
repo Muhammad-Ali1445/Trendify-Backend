b91bf1a35abeb226c114921ae9565f48aee0c158
Ali Munir <alimunir1445@gmail.com>
2024-08-12 15:11:39 +0500

    change port

--- db/index.js
+++ db/index.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://abubakarch705:heIawZVATFLPnuHE@khareedo.xth9bzt.mongodb.net/?retryWrites=true&w=majority'
+      // 'mongodb+srv://abubakarch705:heIawZVATFLPnuHE@khareedo.xth9bzt.mongodb.net/?retryWrites=true&w=majority'
+      "mongodb+srv://alimunir1445:qjXCU5PbQLm8JDyM@alimongos123.zncvv.mongodb.net/?retryWrites=true&w=majority&appName=AliMongos123"
     );
-    console.log('Connected to db!');
+    console.log("Connected to db!");
   } catch (error) {
-    console.log('DB Connection', error);
+    console.log("DB Connection", error);
   }
 };
 
